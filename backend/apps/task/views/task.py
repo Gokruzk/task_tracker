0415d68db076b2c4536c1895fb6b97e6f824a7f3
gokruzk <nigelljama@gmail.com>
2026-03-04 09:25:34 -0500

    feat: :sparkles: pagination

--- backend/apps/task/views/task.py
+++ backend/apps/task/views/task.py
@@ -1,17 +1,23 @@
-from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.pagination import PageNumberPagination
+from django_filters.rest_framework import DjangoFilterBackend
 
 from apps.task.models import Task
 from apps.task.serializers.task import TaskSerializer
 from apps.task.views.filters import TaskFilter
 
+class TaskPagination(PageNumberPagination):
+    page_size = 5
+    page_size_query_param = "page_size"  # e.g. ?page_size=20
+    max_page_size = 50
 
 class TaskViewSet(viewsets.ModelViewSet):
     serializer_class = TaskSerializer
     permission_classes = [IsAuthenticated]
     filter_backends = [DjangoFilterBackend]
     filterset_class = TaskFilter
+    pagination_class = TaskPagination
 
     def get_queryset(self):
         qs = Task.objects.select_related("status")
