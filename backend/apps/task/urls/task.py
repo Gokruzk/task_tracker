046f7e5199b85fe1553460b1ad42357ae48054bf
gokruzk <nigelljama@gmail.com>
2026-03-03 23:38:28 -0500

    refactor: :adhesive_bandage: remove slash at the end of the endpoint

--- backend/apps/task/urls/task.py
+++ backend/apps/task/urls/task.py
@@ -1,7 +1,7 @@
 from rest_framework.routers import DefaultRouter
 from apps.task.views.task import TaskViewSet
 
-router = DefaultRouter()
+router = DefaultRouter(trailing_slash=False)
 router.register(r"tasks", TaskViewSet, basename="tasks")
 
 urlpatterns = router.urls
--- backend/apps/users/urls.py
+++ backend/apps/users/urls.py
@@ -1,7 +1,7 @@
 from rest_framework.routers import DefaultRouter
 from apps.users.views import UserViewSet
 
-router = DefaultRouter()
+router = DefaultRouter(trailing_slash=False)
 router.register(r"users", UserViewSet, basename="users")
 
 urlpatterns = router.urls
