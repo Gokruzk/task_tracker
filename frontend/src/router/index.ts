a1761bfaf9eb8c901a7f953435a06398362a7b98
gokruzk <nigelljama@gmail.com>
2026-03-04 11:06:26 -0500

    feat: :tada: begin vue project

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,8 +1,21 @@
+/**
+ * router/index.ts
+ *
+ * Manual routes for ./src/pages/*.vue
+ */
+
+// Composables
 import { createRouter, createWebHistory } from 'vue-router'
+import Index from '@/pages/index.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      component: Index,
+    },
+  ],
 })
 
 export default router
