fb4f3d13597f5408fbe5ba9cbcd9f0eee71027fb
gokruzk <nigelljama@gmail.com>
2026-03-03 23:34:42 -0500

    feat: :sparkles: users endpoints

--- backend/apps/users/views.py
+++ backend/apps/users/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
-# Create your views here.
+from apps.users.models import User
+from apps.users.serializers import UserSerializer, UserCreateSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+
+    def get_permissions(self):
+        # Permitir registro sin autenticación
+        if self.action == "create":
+            return []
+        return [IsAuthenticated()]
+
+    def get_serializer_class(self):
+        if self.action == "create":
+            return UserCreateSerializer
+        return UserSerializer
+
+    def get_queryset(self):
+        # Solo devuelve el usuario autenticado
+        if self.request.user.is_authenticated:
+            return User.objects.filter(id=self.request.user.id)
+        return User.objects.none()
+
+    def perform_update(self, serializer):
+        serializer.save()
+
+    @action(detail=False, methods=["get"])
+    def me(self, request):
+        serializer = self.get_serializer(request.user)
+        return Response(serializer.data)
